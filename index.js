6d6b768f9bb0584c027b476fbf67c4ae2537af25
Julio Henrique <juliohmb@gmail.com>
2020-04-19 19:12:05 -0300

    add slow-stop, continue feature

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const fireHeight = 45
 const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 var debug = false
 var wind = true
-var interval
 
-fireDecay = 3
+var interval
+var stopnterval
+
+const fireDecay = 3
 
 var running = true
 
@@ -14,10 +16,29 @@ function start(){
     createFireDataStructure()
     setSource()
     render()
+    clearInterval(stopnterval)
+    stopnterval = null
+    clearInterval(interval)
+    interval = null
     if(interval == null){
         interval = setInterval(calculateFirePropagation, 50)
     }
     running = true
+}
+
+function stop(){
+    let max = firePixelsArray[fireHeight * fireWidth - 1]
+    console.log(max)
+    stopnterval = setInterval(() => {
+        console.log("onstopinter")
+        if(max <= 0){
+            clearInterval(stopnterval)
+            stopnterval = null
+        }else{
+            max -= 1
+            setSource(max)
+        }
+    }, 50)
 }
 
 function pause(){
@@ -25,6 +46,14 @@ function pause(){
         running = false
         clearInterval(interval)
         interval = null
+    }
+}
+
+function play(){
+    if(running === false){
+        running = true
+        calculateFirePropagation()
+        interval = setInterval(calculateFirePropagation, 50)
     }
 }
 
@@ -36,11 +65,11 @@ function createFireDataStructure(){
     }
 }
 
-function setSource(){
+function setSource(intensity = 36){
     const startIndexSource = (fireHeight * fireWidth) - fireWidth
     const endIndexSource = (fireHeight * fireWidth)
     for(let i = startIndexSource; i < endIndexSource; i++){
-        firePixelsArray[i] = 36
+        firePixelsArray[i] = intensity
     }
 }
 
