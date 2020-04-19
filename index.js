4dddfc8ceff7963004a75f1c288f58c6d8e38142
Julio Henrique <juliohmb@gmail.com>
2020-04-19 12:06:19 -0300

    Canvas updatw

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const firePixelsArray = []
 const fireWidth = 40
 const fireHeight = 40
-
 const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
-
 var debug = false
 var wind = true
-
 var interval
 
 var running = true
@@ -67,15 +64,18 @@ function render(){
                 let index = i * fireWidth + j
                 if (debug === true){
                     table += `<td><span>${firePixelsArray[index]}</span ><span class="index">${index}</span></td>`
-                }else{
-                    table += `<td class="debugOff" style="background-color:rgb(${fireColorsPalette[firePixelsArray[index]].r}, ${fireColorsPalette[firePixelsArray[index]].g}, ${fireColorsPalette[firePixelsArray[index]].b})"></td>`
+                }
+                else{
+                    const context = document.querySelector("canvas").getContext("2d")
+                    context.fillStyle = `rgb(${fireColorsPalette[firePixelsArray[index]].r},${fireColorsPalette[firePixelsArray[index]].g},${fireColorsPalette[firePixelsArray[index]].b})`
+                    context.fillRect(j, i, 1, 1)
                 }
             }
             table += "</tr>"
         }
         table += "</table>"
-        document.querySelector("#canvas").innerHTML = table
-}
+        document.querySelector("#canvasDiv").innerHTML = table
+}   
 
 function toggleWind(){
     wind === true?wind = false:wind = true
